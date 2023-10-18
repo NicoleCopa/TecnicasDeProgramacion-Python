f3c230b125a92f64394962417cb7e75ccb40b1e6
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2023-10-18 17:57:18 -0300

    Update ejercicio01.py

--- TP/Python/ejercicio01.py
+++ TP/Python/ejercicio01.py
@@ -1,35 +1,13 @@
-"""
-Escribir un algoritmo que permita al usuario ingresar 2 números y luego le indique el 
-resultado de la suma de esos dos números.
-_______________________________________
-ENTRADA                 IDENTIFICADOR
-    Numero 1                num1
-    Numero 2                num2
-SALIDA
-    Suma                    sum
-_______________________________________
-"""
+"""Escribir un algoritmo que pida al usuario dos números y muestre en pantalla 
+la suma de ambos."""
 
-#Decoracion: Nombre del algorítmo
-print("-------------------------------------------------------")
-print("           EJERCICIO 1: CALCULAR SUMA.")
-print("-------------------------------------------------------")
+print("""-------------------------------------------------------)
+                EJERCICIO 1: CALCULAR SUMA.
+-------------------------------------------------------""")
 
-#Entrada
 num1 = int(input("Ingrese un número: "))
 num2 = int(input("Ingrese un segundo número: "))
 
-#Proceso
 sum = num1 + num2
 
-#Salida
 print("El resultado de ambos números es:", sum)
-
-"""
--------------------------------------------------------
-            EJERCICIO 1: CALCULAR SUMA
--------------------------------------------------------
-Ingrese un número: 20
-Ingrese un segundo número: 10
-El resultado de ambos números es: 30
-"""

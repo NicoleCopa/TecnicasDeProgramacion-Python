159fa5d6af589da470da2b12a21978966976564b
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2023-10-18 17:57:59 -0300

    Update ejercicio03.py

--- TP/Python/ejercicio03.py
+++ TP/Python/ejercicio03.py
@@ -1,36 +1,12 @@
-"""
-Escribir un algoritmo que permita al usuario calcular una potencia y luego muestre el 
-resultado de este cálculo. Para ello el usuario debe ingresar un número y luego su exponente.
-Es decir. Ejemplo: 5**2 = 25 (5 elevado al cuadrado, es igual a 25) 
-_______________________________________
-ENTRADA                 IDENTIFICADOR
-    Numero                  num
-    Exponente               exp
-SALIDA
-    Potencia                pot
-_______________________________________
-"""
+"""Escribir un algoritmo que convierta una temperatura en grados Celsius a Fahrenheit. 
+El usuario debe ingresar la temperatura en grados Celsius. Para ello, deberá multiplicar 
+la temperatura por 1.8 y sumarle 32 para obtener la temperatura en grados Fahrenheit."""
 
-#Decoracion: Nombre del algorítmo
-print("-------------------------------------------------------")
-print("         EJERCICIO 3: CÁLCULO DE POTENCIA")
-print("-------------------------------------------------------")
+print("""-------------------------------------------------------
+                EJERCICIO 3: CELCIUS A FAHRENHEIT
+-------------------------------------------------------""")
 
-#Entrada
-num = int(input("Ingrese un número: "))
-exp = int(input("Ingrese su exponente: "))
+celcius = float(input("Ingrese la temperatura en grados Celsius: "))
+fahrenheit = (celcius * 1.8) + 32
 
-#Proceso
-pot = num**exp
-
-#Salida
-print("La potencia del número es:", pot)
-
-"""
--------------------------------------------------------
-           EJERCICIO 3: CÁLCULO DE POTENCIA
--------------------------------------------------------
-Ingrese un número: 5
-Ingrese su exponente: 2
-La potencia del número es: 25
-"""
+print(f"La temperatura en grados Fahrenheit es {fahrenheit}")

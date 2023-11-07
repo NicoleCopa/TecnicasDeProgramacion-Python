c4a9b5e614ec20729558d4acfcaf9ec115b3412f
Nicole Copa <nicoleemilcecopa@gmail.com>
2023-11-07 12:38:38 -0300

    comenzando ejercicio 25

--- Python/ejercicio25.py
+++ Python/ejercicio25.py
@@ -4,3 +4,12 @@ desordenado elimine todos los valores que se repitan del mismo. Una vez eliminad
 valores deberá informar cuantas eliminaciones hizo, por ejemplo: “Se han eliminado 8 veces el
 mismo valor”. Por ultimo deberá preguntar al usuario si desea eliminar otro valor o no.
 """
+
+import random
+
+valores = []
+
+for i in range(100):
+    valor = random.randint(0,50)
+    valores.append(valor)
+

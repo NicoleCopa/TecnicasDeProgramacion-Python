b4cb4084255e288523f9f653dfb59f90c096763c
Nicole Copa <nicoleemilcecopa@gmail.com>
2023-11-07 12:52:14 -0300

    finalizamos ejercicio 25

--- Python/ejercicio25.py
+++ Python/ejercicio25.py
@@ -7,9 +7,37 @@ mismo valor”. Por ultimo deberá preguntar al usuario si desea eliminar otro v
 
 import random
 
+#declaramos 
 valores = []
+opcion = str()
+contador = 0
 
-for i in range(100):
-    valor = random.randint(0,50)
+#asignamos valores random al array
+for i in range(20):
+    valor = random.randint(0,10)
     valores.append(valor)
 
+#imprimimos el array
+print(valores)
+
+#creamos un while para que se vuelva a ejecutar hasta que no quieran eliminar más valores
+while opcion.lower() != "n":
+    #pedimos ingresar un valor a eliminar
+    valor_eliminado = int(input("Ingrese un valor entero a eliminar: "))
+
+    #recorremos la lista
+    for f in valores:
+        #si el valor se encuentra en la lista...
+        if valor_eliminado in valores:
+            #borramos el valor
+            valores.remove(valor_eliminado)
+            #sumamos un valor al contador
+            contador = contador + 1
+
+    #imprimimos por pantalla el valor a eliminar y la cantidad de veces eliminado
+    print(f"El valor {valor_eliminado} se ha eliminado {contador} veces.")
+    
+    #preguntamos si se desea eliminar otro valor o no
+    opcion = str(input("¿Desea eliminar otro valor? s/n: "))
+
+print("- Fin del programa -")
